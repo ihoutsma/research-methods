63a9ec6e8f2b56c13014b5b6b650a094b70e795e
ihoutsma <124591963+ihoutsma@users.noreply.github.com>
2023-03-26 18:08:03 +0200

    Add files via upload

---
+++ code_ResearchMethods.py
@@ -0,0 +1,23 @@
+import sys
+import json
+import gzip
+
+# Load the JSON file containing the Twitter data
+with gzip.open(sys.argv[1], 'rt') as f:
+    tweets = json.load(f)
+
+# Define the word
+word_to_search = sys.argv[2]
+
+# Initialize the counter
+count = 0
+
+# Loop through each tweet in the data
+for tweet in tweets:
+    # Check if the word is in the tweet text
+    if word_to_search in tweet['text']:
+        # If it is, increment the counter
+        count += 1
+
+# Print the total count of occurrences of the word
+print(f'The word "{word_to_search}" appears {count} times in the dataset.')
